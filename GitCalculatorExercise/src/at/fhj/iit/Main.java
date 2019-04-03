73f8c7770dae90ef4505b1bae0a6dc0175498849
Martin Zellner <martin.zellner@edu.campus02.at>
2019-04-03 10:07:26 +0200

    fixes of add method and getMinimum

--- GitCalculatorExercise/src/at/fhj/iit/Main.java
+++ GitCalculatorExercise/src/at/fhj/iit/Main.java
@@ -1,21 +1,47 @@
 package at.fhj.iit;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Calculator calc = new CalculatorImpl();
+		Calculator calc = new Calculator() {
+			@Override
+			public int sum() {
+				return 0;
+			}
+
+			@Override
+			public int getMaximum() {
+				return 0;
+			}
+
+			@Override
+			public int getMinimum() {
+				int min = 0;
+				return min;
+			}
+
+			@Override
+			public void addValue(int value) {
+				ArrayList c = new ArrayList();
+				c.add(value);
+			}
+		};
 		
-		calc.add(20);	// currently highest number
-		calc.add(4);	// currently lowester number
-		calc.add(16);
+		calc.addValue(20);	// currently highest number
+		calc.addValue(4);	// currently lowester number
+		calc.addValue(16);
 		
 		// add other integers
-		addValue();
+		calc.addValue(13);
+		calc.addValue(5);
+		calc.addValue(23);
 		// output lowest number 
-		getMinimum(calc);
+		calc.getMinimum();
 		// output highest number
-		getMaximum();
+		calc.getMaximum();
 		// output sum of all numbers
 
 	}
